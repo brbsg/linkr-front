5b12528ab6d82a8ca82fd203952af15a0d9ae9e5
barbosaoli <gustavobarbosa7162@gmail.com>
2022-03-25 15:11:44 -0300

    fix bugs

--- src/pages/Timeline/PostBox/index.js
+++ src/pages/Timeline/PostBox/index.js
@@ -9,9 +9,9 @@ export default function Posts() {
   const [reload, setReload] = useState(false);
   const { token } = useAuth;
 
-  setInterval(() => {
-    setReload(!reload);
-  }, 10000);
+  // setInterval(() => {
+  //   setReload(!reload);
+  // }, 10000);
 
   useEffect(() => {
     const promise = api.getPosts(token);

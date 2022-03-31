374a17c81fdfae3714c51996ff4e8a935fb95f95
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-03-31 15:49:33 -0300

    feat: add comment number to post

--- src/components/InteractBar/Comment/index.js
+++ src/components/InteractBar/Comment/index.js
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
 import { AiOutlineComment } from 'react-icons/ai'
 
-export default function Comment({commentsOpen, setCommentsOpen}) {
-    
+import useAuth from '../../../hooks/useAuth';
+import api from '../../../services/api'
+
+export default function Comment({commentsOpen, setCommentsOpen, postId}) {
+    const { token } = useAuth()
+    const [commentData, setCommentData] = useState(null)
+
+    async function getComments() {
+        try {
+            const { data } = await api.getComments(postId, token);
+            setCommentData(data)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+    getComments()
 
     return(
         <div onClick={() => setCommentsOpen(!commentsOpen)}>
         <AiOutlineComment size={30} />
-        <p>comments</p>
+        <p> {
+        commentData ?
+            commentData.length === 1 ? `${commentData.length} comment`  : `${commentData.length} comments`
+        : '0 comments '}</p>
         </div>
     )
 }
